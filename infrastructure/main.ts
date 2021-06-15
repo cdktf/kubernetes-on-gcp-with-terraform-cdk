f3855a62b416055513fe1b1969631df7a9d726aa
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-06-15 16:53:21 +0200

    refactor to use constructs

--- infrastructure/main.ts
+++ infrastructure/main.ts
@@ -1,16 +1,18 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, ITerraformDependable, TerraformStack } from "cdktf";
 import { DockerProvider } from "./.gen/providers/docker/docker-provider";
 import { KubernetesProvider } from "./.gen/providers/kubernetes/kubernetes-provider";
 import * as fs from "fs";
 import * as path from "path";
 import { Namespace } from "./.gen/providers/kubernetes/namespace";
 import { CLUSTER_NAME } from "./config";
-import { application } from "./services";
+import { buildAndPushImage } from "./services";
 import { TerraformGoogleModulesKubernetesEngineGoogleModulesAuth as GKEAuth } from "./.gen/modules/terraform-google-modules/kubernetes-engine/google/modules/auth";
-import { File } from "./.gen/providers/local/file";
 import { HelmProvider } from "./.gen/providers/helm/helm-provider";
-import { Release } from "./.gen/providers/helm/release";
+import { Release, ReleaseConfig } from "./.gen/providers/helm/release";
+import { Resource } from "./.gen/providers/null/resource";
+import { Deployment } from "./.gen/providers/kubernetes/deployment";
+import { Service } from "./.gen/providers/kubernetes/service";
 import {
   ContainerCluster,
   ContainerNodePool,
@@ -21,6 +23,7 @@ import {
   ServiceAccount,
 } from "@cdktf/provider-google";
 
+// https://developers.google.com/identity/protocols/oauth2/scopes
 const oauthScopes = [
   "https://www.googleapis.com/auth/devstorage.read_only",
   "https://www.googleapis.com/auth/logging.write",
@@ -31,44 +34,203 @@ const oauthScopes = [
   "https://www.googleapis.com/auth/cloud-platform",
 ];
 
-function useGoogle(scope: Construct) {
-  new GoogleProvider(scope, "providers", {
-    zone: "us-central1-c",
-    project: "dschmidt-cdk-test",
-  });
+class KubernetesService extends Resource {
+  constructor(
+    scope: Construct,
+    namespace: Namespace,
+    name: string,
+    image: string,
+    labels: Record<string, string>,
+    dependencies: ITerraformDependable[]
+  ) {
+    super(scope, name);
+    const deployment = new Deployment(scope, `${image}-deployment`, {
+      dependsOn: dependencies,
+      metadata: [
+        {
+          name,
+          labels,
+          namespace: namespace.id,
+        },
+      ],
+      spec: [
+        {
+          selector: [
+            {
+              matchLabels: labels,
+            },
+          ],
+          template: [
+            {
+              metadata: [
+                {
+                  labels,
+                },
+              ],
+              spec: [
+                {
+                  container: [
+                    {
+                      name: "application",
+                      image: image,
+                      port: [{ containerPort: 80 }],
+                      livenessProbe: [
+                        {
+                          httpGet: [
+                            {
+                              path: "/health",
+                              port: "80",
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+
+    new Service(scope, `${image}-service`, {
+      dependsOn: [deployment],
+      metadata: [{ name: image, namespace: namespace.id }],
+      spec: [
+        {
+          selector: { application: image },
+          port: [{ port: 80 }],
+        },
+      ],
+    });
+  }
 }
 
-function useCluster(scope: Construct, name: string) {
-  useGoogle(scope);
-  const cluster = new DataGoogleContainerCluster(scope, "cluster", {
-    name,
-  });
+class KubernetesCluster extends Resource {
+  private sa: ServiceAccount;
+  private cluster: ContainerCluster;
 
-  const auth = new GKEAuth(scope, "auth", {
-    clusterName: cluster.name,
-    location: cluster.location,
-    projectId: cluster.project,
-  });
+  constructor(scope: Construct, name: string, serviceAccount: ServiceAccount) {
+    super(scope, name);
 
-  new File(scope, "kubeconfig", {
-    filename: path.resolve(__dirname, "../kubeconfig.yaml"),
-    content: auth.kubeconfigRawOutput,
-  });
+    this.sa = serviceAccount;
+    this.cluster = new ContainerCluster(this, "cluster", {
+      name,
+      removeDefaultNodePool: true,
+      initialNodeCount: 1,
+      nodeConfig: [
+        {
+          preemptible: true,
+          serviceAccount: this.sa.email,
+          oauthScopes,
+        },
+      ],
+    });
+  }
 
-  new KubernetesProvider(scope, "kubernetes", {
-    clusterCaCertificate: auth.clusterCaCertificateOutput,
-    host: auth.hostOutput,
-    token: auth.tokenOutput,
-  });
+  addNodePool(name: string, nodeCount = 3, machineType = "e2-medium") {
+    new ContainerNodePool(this, name, {
+      name,
+      cluster: this.cluster.name,
+      nodeCount,
+      nodeConfig: [
+        {
+          preemptible: true,
+          machineType,
+          serviceAccount: this.sa.email,
+          oauthScopes,
+        },
+      ],
+    });
+  }
 
-  return auth;
+  addAutoscalingNodePool(
+    name: string,
+    minNodeCount = 3,
+    maxNodeCount = 10,
+    machineType = "e2-medium"
+  ) {
+    new ContainerNodePool(this, name, {
+      name,
+      cluster: this.cluster.name,
+      autoscaling: [
+        {
+          minNodeCount,
+          maxNodeCount,
+        },
+      ],
+      nodeConfig: [
+        {
+          preemptible: true,
+          machineType,
+          serviceAccount: this.sa.email,
+          oauthScopes,
+        },
+      ],
+    });
+  }
+
+  static onCluster(scope: Construct, name: string) {
+    const cluster = new DataGoogleContainerCluster(scope, "cluster", {
+      name,
+    });
+
+    const auth = new GKEAuth(scope, "auth", {
+      clusterName: cluster.name,
+      location: cluster.location,
+      projectId: cluster.project,
+    });
+
+    new KubernetesProvider(scope, "kubernetes", {
+      clusterCaCertificate: auth.clusterCaCertificateOutput,
+      host: auth.hostOutput,
+      token: auth.tokenOutput,
+    });
+
+    new HelmProvider(scope, "helm", {
+      kubernetes: [
+        {
+          clusterCaCertificate: auth.clusterCaCertificateOutput,
+          host: auth.hostOutput,
+          token: auth.tokenOutput,
+        },
+      ],
+    });
+
+    return {
+      installHelmChart(config: ReleaseConfig) {
+        new Release(scope, config.name, config);
+      },
+
+      exposeDeployment(
+        namespace: Namespace,
+        name: string,
+        image: string,
+        labels: Record<string, string>,
+        dependencies: ITerraformDependable[]
+      ) {
+        return new KubernetesService(
+          scope,
+          namespace,
+          name,
+          image,
+          labels,
+          dependencies
+        );
+      },
+    };
+  }
 }
 
 class InfrastructureLayer extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    useGoogle(this);
+    new GoogleProvider(this, "google", {
+      zone: "us-central1-c",
+      project: "dschmidt-cdk-test",
+    });
 
     const sa = new ServiceAccount(this, "sa", {
       accountId: "cluster-admin",
@@ -92,71 +254,22 @@ class InfrastructureLayer extends TerraformStack {
 
     new ContainerRegistry(this, "registry", {});
 
-    const cluster = new ContainerCluster(this, "cluster", {
-      name: CLUSTER_NAME,
-      removeDefaultNodePool: true,
-      initialNodeCount: 1,
-      nodeConfig: [
-        {
-          preemptible: true,
-          serviceAccount: sa.email,
-          oauthScopes,
-        },
-      ],
-    });
-
-    new ContainerNodePool(this, "main-pool", {
-      name: "main",
-      cluster: cluster.name,
-      nodeCount: 3,
-      nodeConfig: [
-        {
-          preemptible: true,
-          machineType: "e2-medium",
-          serviceAccount: sa.email,
-          oauthScopes,
-        },
-      ],
-    });
-
-    new ContainerNodePool(this, "workload-pool", {
-      name: "workload",
-      cluster: cluster.name,
-      autoscaling: [
-        {
-          minNodeCount: 1,
-          maxNodeCount: 10,
-        },
-      ],
-      nodeConfig: [
-        {
-          preemptible: true,
-          machineType: "e2-medium",
-          serviceAccount: sa.email,
-          oauthScopes,
-        },
-      ],
-    });
+    const cluster = new KubernetesCluster(this, CLUSTER_NAME, sa);
+    cluster.addNodePool("main");
+    cluster.addAutoscalingNodePool("workloads");
   }
 }
 
 class BaselineLayer extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
-
-    const auth = useCluster(this, CLUSTER_NAME);
-
-    new HelmProvider(this, "helm", {
-      kubernetes: [
-        {
-          clusterCaCertificate: auth.clusterCaCertificateOutput,
-          host: auth.hostOutput,
-          token: auth.tokenOutput,
-        },
-      ],
+    new GoogleProvider(this, "google", {
+      zone: "us-central1-c",
+      project: "dschmidt-cdk-test",
     });
 
-    new Release(this, "cert-manager", {
+    const cluster = KubernetesCluster.onCluster(this, CLUSTER_NAME);
+    cluster.installHelmChart({
       name: "cert-manager",
       repository: "https://charts.jetstack.io",
       chart: "cert-manager",
@@ -170,9 +283,12 @@ class BaselineLayer extends TerraformStack {
 class ApplicationLayer extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
-    useCluster(this, CLUSTER_NAME);
-
+    new GoogleProvider(this, "google", {
+      zone: "us-central1-c",
+      project: "dschmidt-cdk-test",
+    });
     new DockerProvider(this, "docker", {});
+    const cluster = KubernetesCluster.onCluster(this, CLUSTER_NAME);
 
     const ns = new Namespace(this, "ns", {
       metadata: [
@@ -183,9 +299,22 @@ class ApplicationLayer extends TerraformStack {
     });
 
     const servicePath = path.resolve(__dirname, "../services");
-    fs.readdirSync(servicePath).forEach((p) =>
-      application(this, path.resolve(servicePath, p), ns)
-    );
+    fs.readdirSync(servicePath).forEach((p) => {
+      const [tag, image] = buildAndPushImage(
+        this,
+        p,
+        path.resolve(servicePath, p)
+      );
+      cluster.exposeDeployment(
+        ns,
+        p,
+        tag,
+        {
+          application: p,
+        },
+        [image]
+      );
+    });
   }
 }
 
--- infrastructure/services.ts
+++ infrastructure/services.ts
@@ -1,18 +1,15 @@
-import { ITerraformDependable, TerraformAsset } from "cdktf";
+import { TerraformAsset } from "cdktf";
 import { Construct } from "constructs";
 import * as fs from "fs";
 import * as path from "path";
-import { Deployment } from "./.gen/providers/kubernetes/deployment";
-import { Service } from "./.gen/providers/kubernetes/service";
 import { VERSION, DOCKER_ORG } from "./config";
 import { Resource } from "./.gen/providers/null/resource";
-import { Namespace } from "./.gen/providers/kubernetes/namespace";
 import {
   DataGoogleServiceAccount,
   ServiceAccountKey,
 } from "@cdktf/provider-google";
 
-function buildAndPushImage(
+export function buildAndPushImage(
   scope: Construct,
   imageName: string,
   p: string
@@ -68,7 +65,6 @@ function buildAndPushImage(
   });
 
   const version = getVersion();
-  
 
   const tag = `gcr.io/${DOCKER_ORG}/${imageName}:${version}-${content.assetHash}`;
   const image = new Resource(scope, _("image"), {
@@ -76,82 +72,9 @@ function buildAndPushImage(
       tag,
     },
   });
-  
 
   const cmd = `echo '${key.privateKey}' | base64 -D | docker login -u _json_key --password-stdin https://gcr.io && docker build ${dockerfileFlag} -t ${tag} ${content.path} && docker push ${tag}`;
   image.addOverride("provisioner.local-exec.command", cmd);
 
   return [tag, image];
-}
-
-function service(
-  scope: Construct,
-  image: string,
-  imageTag: string,
-  ns: Namespace,
-  dependencies: ITerraformDependable[]
-) {
-  const labels = { application: image, deployedBy: "cdktf" };
-  const deployment = new Deployment(scope, `${image}-deployment`, {
-    dependsOn: dependencies,
-    metadata: [
-      {
-        name: image,
-        labels,
-        namespace: ns.id,
-      },
-    ],
-    spec: [
-      {
-        selector: [
-          {
-            matchLabels: labels,
-          },
-        ],
-        template: [
-          {
-            metadata: [
-              {
-                labels,
-              },
-            ],
-            spec: [
-              {
-                container: [
-                  {
-                    name: "application",
-                    image: imageTag,
-                    port: [{ containerPort: 80 }],
-                    livenessProbe: [{
-                      httpGet: [{
-                        path: "/health",
-                        port: "80",
-                      }]
-                    }]
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  });
-
-  new Service(scope, `${image}-service`, {
-    dependsOn: [...dependencies, deployment],
-    metadata: [{ name: image, namespace: ns.id }],
-    spec: [
-      {
-        selector: { application: image },
-        port: [{ port: 80 }],
-      },
-    ],
-  });
-}
-
-export function application(scope: Construct, p: string, ns: Namespace) {
-  const name = path.basename(p);
-  const [image, resource] = buildAndPushImage(scope, name, p);
-  service(scope, name, image, ns, [ns, resource]);
 }
